25e4d0d3e6b9154bf5cfec785a25b39d7c673b76
Emilien <emilien.ginko@gmail.com>
2025-10-15 16:05:39 +0200

    fix(towerScene): fog working but not with shader

--- src/worlds/TowerScene.ts
+++ src/worlds/TowerScene.ts
@@ -6,6 +6,7 @@ import World from "../classes/World";
 import Tower from "../components/Tower"
 import Medusa from "../components/Medusa";
 
+import {fogPalette} from "../common/colors"
 const colors = {
     grey: "",
     gold: ""
@@ -21,39 +22,47 @@ export default class TwoerScene extends World {
     private gui: GUI
     private medusa: Medusa
     private tweakParams = {
-        fogDistance: 1024,
+        fogDensity: 1,
+        lightX: 50,
+        lightY: 50,
+        lightZ: 50,
     }
     private medusaParams = {
         noiseFactor: 0.1,
         amountOfMedusa: 5,
-
     }
+
+    private sunmaterial: THREE.MeshBasicMaterial
+    private sungeometry: THREE.SphereGeometry
+    private sunmesh: THREE.Mesh
 
     constructor(exp: Experience) {
         super();
         this.exp = exp;
         this.scene = exp.scene
         this.gui = this.exp.helpers.GUI
-        this.alight = new THREE.AmbientLight()
+        this.alight = new THREE.AmbientLight(0xffffff, 1)
+        this.scene.add(this.alight)
 
-        this.light = new THREE.DirectionalLight(0xffe6cc, 1)
+        this.light = new THREE.DirectionalLight(0xffffff, 1)
         this.light.castShadow = true
-        this.light.position.x = 100
-        this.light.position.y = 100
-        this.light.position.z = 100
-        this.scene.add(this.alight)
-
-        this.light2 = new THREE.DirectionalLight(0xffe6cc, 1)
-        this.light2.castShadow = true
-        this.light2.position.x = -100
-        this.light2.position.y = 100
-        this.light2.position.z = -100
-        this.scene.add(this.alight)
+        this.light.position.x = this.tweakParams.lightX
+        this.light.position.y = this.tweakParams.lightY
+        this.light.position.z = this.tweakParams.lightZ
         this.scene.add(this.light)
-        this.scene.add(this.light2)
+
+        this.scene.background = new THREE.Color(fogPalette[0])
+        this.sunmaterial = new THREE.MeshBasicMaterial({ color: "red" })
+        this.sungeometry = new THREE.SphereGeometry(1, 32, 16)
+        this.sunmesh = new THREE.Mesh(this.sungeometry, this.sunmaterial)
+        this.sunmesh.position.x = this.tweakParams.lightX
+        this.sunmesh.position.y = this.tweakParams.lightY
+        this.sunmesh.position.z = this.tweakParams.lightZ
+        this.scene.add(this.sunmesh)
 
 
-        this.scene.fog = new THREE.Fog(0x529467, 32, this.tweakParams.fogDistance)
+        // this.scene.fog = new THREE.Fog(0x529467, 0, this.tweakParams.fogDistance)
+        this.scene.fog = new THREE.FogExp2(0xcccccc, 0.0025);
         this.medusa = new Medusa(this.exp, this.medusaParams)
         this.towers = new Tower(this.exp, {
             base: 8,
@@ -76,14 +85,39 @@ export default class TwoerScene extends World {
     }
     createTweak() {
 
+        const folder = this.gui.addFolder("sun")
+
+        folder.add(
+            this.tweakParams,
+            'lightX',
+            1, 1000, 1
+        ).onChange((e) => {
+            this.light.position.x = e
+            this.sunmesh.position.x = e
+        })
+        folder.add(
+            this.tweakParams,
+            'lightY',
+            1, 1000, 1
+        ).onChange((e) => {
+            this.light.position.y = e
+            this.sunmesh.position.y = e
+        })
+        folder.add(
+            this.tweakParams,
+            'lightZ',
+            1, 1000, 1
+        ).onChange((e) => {
+            this.light.position.z = e
+            this.sunmesh.position.z = e
+        })
+
         this.gui.add(
             this.tweakParams,
-            'fogDistance',
-            1, 4048, 1
+            'fogDensity',
+            0, 1, .001
         ).onChange((e) => {
-
-            this.scene.fog = new THREE.Fog(0x529467, 32, this.tweakParams.fogDistance)
-
+            this.scene.fog = new THREE.FogExp2(fogPalette[0], this.tweakParams.fogDensity)
         })
 
     }
