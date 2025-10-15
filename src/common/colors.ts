0970ef445b53844fe7e3893d3063889b56c482d3
Emilien <emilien.ginko@gmail.com>
2025-10-15 14:32:32 +0200

    chore(project): add color palettes

---
+++ src/common/colors.ts
@@ -0,0 +1,54 @@
+export const waterPalette = [
+    0x50D7E2,
+    0x12DFE9,
+    0x3AF2F6,
+    0x9CFEFF,
+]
+export const rockPalette = [
+    0x266793,
+    0x334E69,
+    0x2E4F76,
+    0x2F5074,
+]
+
+export const grassPalette = [
+    0x3B564B,
+    0x529467,
+    0x63AD7B,
+    0x98B48A,
+]
+
+
+//1st color is for the nageoires
+//2nd color is for the bulb
+export const jellyFishPalette = [
+    0xF9B7FD,
+    0x6F8BE6
+]
+
+
+
+
+
+const background = 0x334E69
+export const fogPalette = [
+    background
+]
+
+export const rendererPalette = [
+    background
+]
+
+const palette = {
+    waterPalette,
+    rockPalette,
+    grassPalette,
+    jellyFishPalette,
+    fogPalette,
+    rendererPalette
+}
+
+
+
+
+export default palette
