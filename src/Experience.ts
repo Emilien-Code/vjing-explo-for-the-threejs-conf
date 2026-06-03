52830374249c0fb62889fde7c1927731390ea935
Emilien <emilien.ginko@gmail.com>
2026-06-03 09:49:52 +0200

    addapt experience

--- src/Experience.ts
+++ src/Experience.ts
@@ -8,11 +8,12 @@ import World from "./classes/World";
 import Helpers from "./utils/Helpers";
 import Ressources from "./utils/Ressources";
 import sources from "./common/sources";
-import Ex1 from "./worlds/ex1";
-import Ex2 from "./worlds/ex2";
-import Ex3 from "./worlds/ex3";
-import TowerScene from "./worlds/TowerScene";
-
+// import TowerScene from "./worlds/TowerScene";
+import ParticlesScene from "./worlds/ParticlesScene";
+import BreakDanceScene from "./worlds/BreakDanceScene";
+import GpgpuScene from "./worlds/sphereScene";
+import AudioManager from "./utils/managers/AudioManager";
+import BPMManager from "./utils/managers/BPMManager";
 export default class Experience {
 
     public canvas: HTMLCanvasElement;
@@ -25,6 +26,10 @@ export default class Experience {
     public renderer: Renderer;
     public helpers: Helpers
     public ressources: Ressources
+
+    public audioManager: AudioManager | undefined
+    public bpmManager: BPMManager | undefined
+    public isAudioLoaded = false
 
     public world: World | null = null; // use the genera Page class type
 
@@ -41,12 +46,38 @@ export default class Experience {
         this.renderer = new Renderer(this);
         this.ressources = new Ressources(sources)
         this.ressources.startLoading()
-        this.createWorld(TowerScene)
+
+
 
         this.sizes.on("resize", () => this.resize());
         this.time.on("tick", () => this.update());
         this.time.tick()
         this.ressources.on('ready', () => this.onReady())
+
+        this.createAudioManagers()
+
+
+    }
+
+    public async createAudioManagers() {
+        this.audioManager = new AudioManager()
+        console.log("start")
+        await this.audioManager.loadAudioBuffer()
+        console.log("start")
+        this.bpmManager = new BPMManager()
+        this.bpmManager.addEventListener('beat', () => {
+            this.world && this.world.onBPMBeat()
+        })
+        /**
+         * double check this.audioManager.audio
+         */
+        await this.bpmManager.detectBPM(this.audioManager.audio.buffer)
+        this.audioManager.play()
+        this.isAudioLoaded = true
+
+        console.log("ready")
+
+
     }
 
     public createWorld(Exp: World) {
@@ -57,7 +88,8 @@ export default class Experience {
 
     }
 
-    onReady(){
+    onReady() {
+        this.createWorld(GpgpuScene)
         this.isReady = true;
         this.world && this.world.onReady()
     }
@@ -70,7 +102,7 @@ export default class Experience {
         this.sizes.viewHeight = Math.tan(this.camera.instance.fov * Math.PI / 180 / 2) * this.camera.instance.position.z * 2;
     }
     public update(): void {
-        if (this.isReady) {
+        if (this.isReady && this.isAudioLoaded) {
             this.camera.update();
             this.renderer.update();
             this.world?.update();
