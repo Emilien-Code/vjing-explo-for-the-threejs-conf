bfeb33d3e94c810d6d49d251c3891d6c45950544
Emilien <emilien.ginko@gmail.com>
2026-06-03 09:50:56 +0200

    add srcs to the app

--- src/common/sources.ts
+++ src/common/sources.ts
@@ -11,7 +11,43 @@ const sources = [
         name: "noise",
         type: "texture",
         path: ["/noise3.png"]
+    }, {
+        name: "break_dance_rig",
+        type: "GLTFModel",
+        path: ["/break_dance_test.glb"]
     },
+    {
+        name: "hiphop_dance_rig",
+        type: "GLTFModel",
+        path: ["/hip_hop_dance.glb"]
+    },
+    {
+        name: "environementMap",
+        type: "texture",
+        path: ["/environment-map.jpg"]
+    },
+    {
+        name: "water_displacement",
+        type: "texture",
+        path: ["/water_displacement.jpeg"]
+    },
+    {
+        name: "glass",
+        type: "GLTFModel",
+        path: ["/glass.glb"]
+    },
+    {
+        name: "pose_falling_1",
+        type: "GLTFModel",
+        path: ["/pose_falling.glb"]
+    },
+    {
+        name: "pose_falling_2",
+        type: "GLTFModel",
+        path: ["/pose_falling_2.glb"]
+    },
+
+
 ]
 
 
